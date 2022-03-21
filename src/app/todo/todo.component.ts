5946ea883dce30c04b4b8fd498a495b1c9d02e7b
Avichay Vaknin <79922086+AV2606@users.noreply.github.com>
2022-03-21 21:07:12 +0200

    finished

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+export interface Task{
+  name:string;
+  isUpdated:boolean;
+}
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
+
 export class TodoComponent implements OnInit {
 
 
-  tasks:string[]=[];
+  tasks:Task[]=[];
 
   constructor() {
     this.tasks=[
-      "task 1",
-      "task 2",
-      "task 3",
+      {name:'task 1',isUpdated:false},
+      {name:'task 2',isUpdated:false},
+      {name:'task 3',isUpdated:false}
     ]
    }
 
@@ -24,15 +30,15 @@ export class TodoComponent implements OnInit {
 
   handleSubmit(addForm:NgForm){
     let newTask=addForm.value.task;
-    this.tasks.push(newTask);
+    this.tasks.push({name:newTask,isUpdated: false});
     addForm.resetForm();
   }
-  handleRemove(task:string){
+  handleRemove(task:Task){
     //this.tasks=this.tasks.filter((myTask)=>myTask!=task);
     this.tasks=this.removeFirst(this.tasks,task);
   }
-  removeFirst(arr:string[],value:string):string[]{
-    let r:string[]=[];
+  removeFirst(arr:any[],value:any):any[]{
+    let r:any[]=[];
     let removed1=false;
       arr.forEach(element => {
         if(element!=value||removed1)
@@ -41,6 +47,13 @@ export class TodoComponent implements OnInit {
           removed1=true;
       });
     return r;
+  }
+  handleUpdate(task:Task){
+    task.isUpdated=true;
+  }
+  handleUpdating(text:string,task:Task){
+    task.isUpdated=false;
+    task.name=text;
   }
 
 }
